e60d328a15c832d793e8632677bf738a54e9719f
icad08 <irsyadrm08@gmail.com>
2024-03-28 14:39:33 +0700

    selesai

--- QuickSort_0227.cpp
+++ QuickSort_0227.cpp
@@ -103,5 +103,12 @@ int main ()
         cin >> ch;
         if (ch == 'n' || ch == 'N')
         break;
+
+        system ("pause");
+        system ("cls");
+
     }
+    while (true);
+
+    return 0;
 }
