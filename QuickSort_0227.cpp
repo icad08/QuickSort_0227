3fdd2f479aec61564cbf0046c9547e3bdc05dded
icad08 <irsyadrm08@gmail.com>
2024-03-28 14:37:48 +0700

    int main

--- QuickSort_0227.cpp
+++ QuickSort_0227.cpp
@@ -88,5 +88,20 @@ void display () {
     }
     cout << "\n nNumber  of comparisons: " << cmp_count << endl;
     cout << "\n number of data movements: " << mov_count << endl;
-    
+
+}
+
+int main ()
+{
+    char ch;
+
+    do {
+        input();
+        q_short(0, n - 1);
+        display();
+        cout << "\n\nDo you want to continue? (y/n); ";
+        cin >> ch;
+        if (ch == 'n' || ch == 'N')
+        break;
+    }
 }
