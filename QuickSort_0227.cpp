299e4f1fb8836ac8d5f936e36b1887c4ce4ea008
icad08 <irsyadrm08@gmail.com>
2024-03-28 14:17:49 +0700

    step 6

--- QuickSort_0227.cpp
+++ QuickSort_0227.cpp
@@ -52,6 +52,9 @@ void q_short (int low, int high)
     while (i = j) // step 10 
     {
         while ((arr[i] <= pivot) && (i <= high)) // step 5
+        {
+            i++; // step 6
+        }
     }
     
 }
